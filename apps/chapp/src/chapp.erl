abadee0dc123ff53a603d2763f286353df2f6a0a
Christopher Biscardi <biscarch@sketcht.com>
2013-01-16 14:14:09 -0800

    some routing

--- apps/chapp/src/chapp.erl
+++ apps/chapp/src/chapp.erl
@@ -3,14 +3,30 @@
 -include_lib("riak_core/include/riak_core_vnode.hrl").
 
 -export([
-         ping/0
+         ping/0,
+         pingsame/0,
+         pinginput/1
         ]).
 
 %% Public API
 
-%% @doc Pings a random vnode to make sure communication is functional
+%% @doc Pings a random vnode 
 ping() ->
     DocIdx = riak_core_util:chash_key({<<"ping">>, term_to_binary(now())}),
+    PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, chapp),
+    [{IndexNode, _Type}] = PrefList,
+    riak_core_vnode_master:sync_spawn_command(IndexNode, ping, chapp_vnode_master).
+
+%% @doc Pings the same vnode every time
+pingsame() ->
+  DocIdx = riak_core_util:chash_key({<<"ping">>, <<"same">>}), 
+    PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, chapp),
+    [{IndexNode, _Type}] = PrefList,
+    riak_core_vnode_master:sync_spawn_command(IndexNode, ping, chapp_vnode_master).
+
+%% @doc Pings the same vnode, based on user input.
+pinginput(X) ->
+  DocIdx = riak_core_util:chash_key({<<"ping">>, term_to_binary(X)}), 
     PrefList = riak_core_apl:get_primary_apl(DocIdx, 1, chapp),
     [{IndexNode, _Type}] = PrefList,
     riak_core_vnode_master:sync_spawn_command(IndexNode, ping, chapp_vnode_master).
--- readme.md
+++ readme.md
@@ -3,4 +3,4 @@
 Revelent Files include [chapp.erl](https://github.com/ChristopherBiscardi/Riak-Core-Consistent-Hash-Routing-Example/blob/master/apps/chapp/src/chapp.erl)
 and [chapp_vnode.erl](https://github.com/ChristopherBiscardi/Riak-Core-Consistent-Hash-Routing-Example/blob/master/apps/chapp/src/chapp_vnode.erl)
 
-
+[blog post](http://www.christopherbiscardi.com/2013/01/16/consistant-hash-routing-in-riak-core/)
